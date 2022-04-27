4f916ac06b9b1460cf4ddd56d65e1fe1196e7b5c
Celalettin Berat Altuntaş <cberataltuntas@gmail.com>
2022-04-27 13:23:34 +0300

    Update Config.swift

--- Marvel Characters/Config.swift
+++ Marvel Characters/Config.swift
@@ -5,10 +5,3 @@
     //  Created by BERAT ALTUNTAŞ on 26.04.2022.
     //
 
-import Foundation
-struct Config{
-    let publicApiKey = "f5bb77ddfb04fec5fc5bf4a95b78f048"
-    let privateApiKey = "d372ae06dc3c3642dfae73088bf84f9b456641b5"
-    let keysWithHash = "ts=thesoer&apikey=f5bb77ddfb04fec5fc5bf4a95b78f048&hash=ac77cae4bb0ef66e6943f30a9163dbf8"
-    let mainUrl = "https://gateway.marvel.com/v1/public/"
-}
