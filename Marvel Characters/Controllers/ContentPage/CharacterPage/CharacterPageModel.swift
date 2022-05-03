2d2c7349eab3d92b0949ab53324ee454798b06de
Berat Altuntaş <cberataltuntas@gmail.com>
2022-05-03 22:41:58 +0300

    character page UI finished.

--- Marvel Characters/Controllers/ContentPage/CharacterPage/CharacterPageModel.swift
+++ Marvel Characters/Controllers/ContentPage/CharacterPage/CharacterPageModel.swift
@@ -1,24 +1,27 @@
-//
-//  CharacterPageModel.swift
-//  Marvel Characters
-//
-//  Created by BERAT ALTUNTAŞ on 1.05.2022.
-//
+    //
+    //  CharacterPageModel.swift
+    //  Marvel Characters
+    //
+    //  Created by BERAT ALTUNTAŞ on 1.05.2022.
+    //
 
 import Foundation
 
+    // MARK: - CharacterPageViewModelProtocol
 protocol CharacterPageViewModelProtocol {
-    
+    var delegate: CharacterPageViewModelDelegate? { get set }
 }
 
-protocol CharacterPageViewModelDelegate {
-    
+    // MARK: - CharacterPageViewModelDelegate
+protocol CharacterPageViewModelDelegate: AnyObject {
 }
 
+    // MARK: - CharacterPageViewModel
 final class CharacterPageViewModel {
-    
+    weak var delegate: CharacterPageViewModelDelegate?
 }
 
+    // MARK: - Extension CharacterPageViewModel
 extension CharacterPageViewModel: CharacterPageViewModelProtocol {
     
 }
--- Marvel Characters/Controllers/ContentPage/CharacterPage/CharacterPageViewController.swift
+++ Marvel Characters/Controllers/ContentPage/CharacterPage/CharacterPageViewController.swift
@@ -1,16 +1,27 @@
-//
-//  CharacterPageViewController.swift
-//  Marvel Characters
-//
-//  Created by BERAT ALTUNTAŞ on 26.04.2022.
-//
+    //
+    //  CharacterPageViewController.swift
+    //  Marvel Characters
+    //
+    //  Created by BERAT ALTUNTAŞ on 26.04.2022.
+    //
 
 import UIKit
 
+    // MARK: - CharacterPageViewController
 final class CharacterPageViewController: BaseViewController {
-
+    
     var character: CharacterModelResult?
+    var viewModel: CharacterPageViewModel! {
+        didSet {
+            viewModel.delegate = self
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+}
+
+    // MARK: - Extension CharacterPageViewController
+extension CharacterPageViewController: CharacterPageViewModelDelegate {
+    
 }
